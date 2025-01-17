69cf04c37d7c22c3a0594f6add693531b3caabe5
kmvandenboss <kevin@vandenbossgroup.com>
2025-01-17 17:26:31 -0500

    updated packages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
   },
   eslint: {
     ignoreDuringBuilds: true,
