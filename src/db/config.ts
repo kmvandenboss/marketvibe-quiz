abb07fe4a2235f452d62476b5789eb405bcefb43
kmvandenboss <kevin@vandenbossgroup.com>
2025-01-20 12:00:37 -0500

    Fixed db config typescript error with neon and drizzl-orm mismatch

--- src/db/config.ts
+++ src/db/config.ts
@@ -11,7 +11,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql as any, { schema });
 
 export const config = {
   connectionString: process.env.DATABASE_URL,
